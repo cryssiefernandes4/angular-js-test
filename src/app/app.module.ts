1345282da87f875499c258a821768f5c7c81f54c
cryssie.fernandes4 <cryssie.fernandes4@gmail.com>
2017-07-11 23:27:07 +0530

    Remove routing from app.modile.ts and add it to a new file specifically for Routing purposes. Decouple the dashboard from the dashboard.component.ts file to a new template html. Decouple the Heroes component template and styling to new component.html and component.css files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
-
 import { RouterModule }   from '@angular/router';
-
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { AppRoutingModule }     from './app-routing.module';
+import { HeroService} from './hero.service';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-  {
-    path: 'heroes',
-    component: HeroesComponent
-  },
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  },
-  {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full'
-  },
-])],
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
     DashboardComponent ],
+  providers: [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
 
