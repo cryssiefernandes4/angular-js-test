a47cac1e28a8a601a856b2b2e554bc42db664382
cryssie.fernandes4 <cryssie.fernandes4@gmail.com>
2017-07-13 23:12:37 +0530

    Retrieve the hero data from a mock remote server using InMemoryWebApiModule and Http Module of Angular JS. Persist the changes back to the remote server.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { RouterModule }   from '@angular/router';
+import { AppRoutingModule }     from './app-routing.module';
+
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
-import { RouterModule }   from '@angular/router';
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { AppRoutingModule }     from './app-routing.module';
+
 import { HeroService} from './hero.service';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
   declarations: [
     AppComponent,
